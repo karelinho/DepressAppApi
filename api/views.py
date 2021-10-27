5df003009e1359a5c8ec76233770922397dd09d1
karelinho <karel.sluka@gmail.com>
2021-10-27 22:32:14 +0200

    Try to repair problem with sending email.

--- api/views.py
+++ api/views.py
@@ -34,13 +34,11 @@ class DepressViewSet(viewsets.ModelViewSet):
             subject = "sledovani priznaku deprese"
             body = "Tento email byl vygenerovan automaticky. Prosim neodpovidejte na nej. V případě dotazu mne kontaktujte na adrese karel.sluka@gmail.com"
             sender_email = "depressapp@gmail.com"
-            # receiver_email = "pa.hvezdova@seznam.cz"
-            receiver_email = "karel.sluka@gmail.com"
+            receiver_email = "pa.hvezdova@seznam.cz"
             message = MIMEMultipart()
             message["From"] = sender_email
             message["To"] = receiver_email
             message["Subject"] = subject
-            # message["Bcc"] = "karel.sluka@gmail.com"
             filename = "karelsluka.xlsx"
             message.attach(MIMEText(body, "plain"))
 
